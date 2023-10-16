a8933b96a797e7f073bec85b6b27bbe14091a4db
Peter Holst Andersen <peter.holst.andersen@gmail.com>
2023-10-16 10:17:42 +0200

    Completed day 18: hot surfaces

--- 2022/surfaces18/surfaces.py
+++ 2022/surfaces18/surfaces.py
@@ -1,57 +1,12 @@
 import sys
 import time
 
-def get_dimension(block):
-    if type(block) is int:
-        return 1
-    else:
-        return len(block)
-
-def get_surfaces_on_block(block):
-    surfaces = None
-    dimension = get_dimension(block)
-    if dimension == 1:
-        x = block
-        surfaces = [ (x-1,x), (x,x+1) ]
-    elif dimension == 2:
-        (x, y) = block
-        surfaces = []
-        for (dx,dy) in [ (-1,0), (0,1), (1,0), (0,-1) ]:
-            surfaces.append( ( (x,y), (x+dx, y+dy) ))
-    elif dimension == 3:
-        (x, y, z) = block
-        surfaces = []
-        for (dx,dy,dz) in [ (-1,0,0), (1,0,0), (0,-1,0), (0,1,0), (0,0,-1), (0,0,1) ]:
-            surfaces.append( ( (x,y,z), (x+dx,y+dy,z+dz) ))
-    else:
-        print("Unhandled dimension: ", len(block))
-        sys.exit(1)
-    return surfaces
-
-def register_surface(surfaces, surface):
-    (coord1, coord2) = surface
-    key = (coord1, coord2) if (coord1 < coord2) else (coord2, coord1)
-    surfaces[key] = surfaces.get(key, 0) + 1
-
-def doit(blocks):
-    surfaces = dict()
-    for block in blocks:
-        for surface in get_surfaces_on_block(block):
-            register_surface(surfaces, surface)
-    free_surfaces = 0
-    for surface in surfaces:
-        if surfaces.get(surface, None) == 1:
-            free_surfaces += 1
-    print(free_surfaces)
-
 def get_neighbours(block):
     (x, y, z) = block
-    neighbours = []
-    for (dx,dy,dz) in [ (-1,0,0), (1,0,0), (0,-1,0), (0,1,0), (0,0,-1), (0,0,1) ]:
-        neighbours.append( (x+dx,y+dy,z+dz) )
-    return neighbours
+    for (dx, dy, dz) in ( (-1,0,0), (1,0,0), (0,-1,0), (0,1,0), (0,0,-1), (0,0,1) ):
+        yield (x+dx, y+dy, z+dz)
 
-def doit2(blocks):
+def count_free_surfaces(blocks):
     count = 0
     for block in blocks:
         for neighbour in get_neighbours(block):
@@ -59,30 +14,61 @@ def doit2(blocks):
                 count += 1
     return count
 
-def example():
-    blocks = [ (2,2,2), (1,2,2), (3,2,2), (2,1,2), (2,3,2), (2,2,1),
-               (2,2,3), (2,2,4), (2,2,6), (1,2,5), (3,2,5), (2,1,5),
-               (2,3,5)
-    ]
-    return blocks
+def count_hot_surfaces(blocks):
+    hot = get_hot_blocks(blocks)
+    count = 0
+    for block in blocks:
+        for neighbour in get_neighbours(block):
+            if not neighbour in blocks and neighbour in hot:
+                count += 1
+    return count
 
-def main():
-    filename = "small.in"; expected_count = 64
-    filename = "big.in";   expected_count = 4332
+def inside(block, minimum, maximum):
+    (x, y, z) = block
+    (x1, y1, z1) = minimum
+    (x2, y2, z2) = maximum
+    return (x1 <= x <= x2) and (y1 <= y <= y2) and (z1 <= z <= z2)
 
-    blocks = []
+def find_min_max(blocks):
+    xs = [ x for (x, _, _) in blocks ]
+    ys = [ y for (_, y, _) in blocks ]
+    zs = [ z for (_, _, z) in blocks ]
+    return ( (min(xs) - 1, min(ys) - 1, min(zs) - 1) , (max(xs) + 1, max(ys) + 2, max(zs) + 2) )
+
+def get_hot_blocks1(block, blocks, minimum, maximum, hot):
+    if block in hot:
+        return
+    hot.add(block)
+    for neighbour in get_neighbours(block):
+        if neighbour not in blocks and inside(neighbour, minimum, maximum):
+            get_hot_blocks1(neighbour, blocks, minimum, maximum, hot)
+
+def get_hot_blocks(blocks):
+    (minimum, maximum) = find_min_max(blocks)
+    hot = set()
+    get_hot_blocks1(minimum, blocks, minimum, maximum, hot)
+    return hot
+
+def read_file(filename):
+    blocks = set()
     with open(filename) as file:
         for line in file:
             if len(line) == 0:
                 continue
             block = line.strip().split(",")
-            blocks.append( (int(block[0]), int(block[1]), int(block[2])) )
+            blocks.add( (int(block[0]), int(block[1]), int(block[2])) )
+    return blocks
 
-    print(f"{filename}: read {len(blocks)} blocks")
-
+def main():
+    sys.setrecursionlimit(10000)
+    # filename = "small.in"; expected_free_count = 64; expected_hot_count = 58
+    filename = "big.in"; expected_free_count = 4332; expected_hot_count = 2524
+    blocks = read_file(filename)
     start_time = time.time()
-    count = doit2(blocks)
-    print(f"count = {count}:", "ok" if count == expected_count else f"not ok: expected {expected_count}")
+    free_count = count_free_surfaces(blocks)
+    hot_count = count_hot_surfaces(blocks)
+    print(f"free_count = {free_count}:", "ok" if free_count == expected_free_count else f"not ok: expected {expected_free_count}")
+    print(f"hot_count = {hot_count}:", "ok" if hot_count == expected_hot_count else f"not ok: expected {expected_hot_count}")
     print("--- %s seconds ---" % (time.time() - start_time))
 
 if __name__ == "__main__":
