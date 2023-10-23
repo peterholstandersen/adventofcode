3970496d9b95fc54f1994b9322ae5ac5b7d80af1
Peter Holst Andersen <peter.holst.andersen@gmail.com>
2023-10-23 21:53:03 +0200

    Work in progress

---
+++ 2022/rope9/rope.py
@@ -0,0 +1,70 @@
+import sys
+from collections import defaultdict
+
+compose = lambda f, g: lambda x: f(g(x))
+
+# Direction functions when applied on (line, col) will return (line1, col1) in the given direction
+R = lambda lc: (lc[0], lc[1] + 1)
+L = lambda lc: (lc[0], lc[1] - 1)
+D = lambda lc: (lc[0] + 1, lc[1])
+U = lambda lc: (lc[0] - 1, lc[1])
+UR = compose(U, R)
+UL = compose(U, L)
+DR = compose(D, R)
+DL = compose(D, L)
+stay = lambda x: x
+_ = stay
+
+
+# Rules for moving the tail. Since we cannot index a list with negative numbers, the indices are offset by 2.
+# tail_move_rules[dl+2, dc+2] specifies which direction the tail shall move in where
+# dl = head_line - tail_line and dc = head_col - tail_col. The _ denotes "stay".
+tail_move_rules = [
+    [UL, UL, U, UR, UR],
+    [UL, _,  _,  _, UR],
+    [ L, _,  _,  _, R ],
+    [DL, _,  _,  _, DR],
+    [DL, DL, D, DR, DR],
+]
+# For easy lookup the rules are stored in a dictionary
+moves = { (l,c): tail_move_rules[l+2][c+2] for l in range(-2,3) for c in range(-2,3) }
+
+with open("big.in") as file:
+    dcs = [ direction_count.split(" ") for direction_count in file.read().strip().split("\n") ]
+    directions = "".join([direction * eval(count) for (direction, count) in dcs])
+
+def part1():
+    visited = set()
+    head = (0, 0)
+    tail = (0, 0)
+    for d in directions:
+        head = eval(f"{d}({head})")
+        tail = moves.get((head[0] - tail[0], head[1] - tail[1]), lambda x: x)(tail)
+        visited.add(tail)
+    print(len(visited))
+
+def part2(length):
+    # body[0] is the head
+    start = (0, 0)
+    body = [ start ] * length
+    visited = { start }
+    for d in directions:
+        body[0] = eval(f"{d}({body[0]})")
+        for i in range(1, length):
+            prev = body[i - 1]
+            this = body[i]
+            body[i] = moves[(prev[0] - this[0], prev[1] - this[1])](this)
+        visited.add(body[-1])
+    print(len(visited))
+
+part2(2)    # is the same as part1()
+part2(10)
+# part1: 6209
+# part2: 2460
+
+def pairs(body):
+    return [(body[i - 1], body[i]) for i in range(1, len(body))]
+
+def right(body):
+    [ R(body[0]) ] + [ moves[(prev[0] - this[0], prev[1] - this[1])](this) for (prev, this) in pairs(body) ]
+
