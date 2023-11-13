5c3a2c906ed66336ef02fe5ec24f3cb1e8779741
Peter Holst Andersen <peter.holst.andersen@gmail.com>
2023-11-13 11:17:22 +0100

    done

--- utils.py
+++ utils.py
@@ -10,6 +10,42 @@ class Timer:
     def __exit__(self, *args):
         print("time:", time.time() - self.start)
 
+class Map():
+    def __init__(self, filename):
+        with open(filename) as file:
+            self.text = file.read().strip("\n")
+            self.lines = self.text.split("\n")
+            self.height = len(self.lines)
+            self.width = self.text.index("\n")
+            self.text = self.text.replace("\n", "")
+            self.rows = [ row for row in range(1, self.height + 1) ]
+            self.cols = [ col for col in range(1, self.width + 1)]
+            self._all = [(row, col) for row in self.rows for col in self.cols]
+            self._map = {(row, col): int(self.text[(row - 1) * self.width + (col - 1)]) for (row, col) in self._all}
+
+    def all(self):
+        return (item for item in self._map.items())
+
+    def __getitem__(self, pos):
+        return self._map[pos]
+
+    def get_neighbours(self, pos, diagonal=False):
+        (row, col) = pos
+        rc = [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)] if diagonal else [(-1,0), (1,0), (0,-1), (0,1)]
+        return [(row + r, col + c) for (r, c) in rc if (row + r, col + c) in self._map]
+
+    def get_some(self, condition):
+        return {pos for pos in self._map if condition(self._map[pos])}
+
+    def for_all(self, f):
+        for pos in self._map:
+            self._map[pos] = f(self._map[pos])
+
+    def __str__(self):
+        f = lambda x: str(9 if x > 9 else x)
+        return '\n'.join([ ''.join([f(self._map[(row, col)]) for col in self.cols]) for row in self.rows])
+
+
 class ansi:
     green = "\u001b[32m"
     white = "\u001b[37m"
